a4ceb0c92b60fcaa6f723ed1029c5e6e08aed075
Sujay Jayakar <sujayakar@convex.dev>
2024-10-15 20:25:25 -0400

    Update build script

--- src/s2-bindings/build.py
+++ src/s2-bindings/build.py
@@ -1,23 +1,27 @@
 import subprocess
 import os
 import base64
+import tempfile
 
 DEBUG = False
 
-subprocess.check_call(["go", "mod", "tidy"])
-assert subprocess.check_output(["tinygo", "version"]).strip().split()[2] == b"0.33.0"
+with tempfile.TemporaryDirectory() as tempdir:
+    subprocess.check_call(["go", "mod", "tidy"])
+    assert subprocess.check_output(["tinygo", "version"]).strip().split()[2] == b"0.33.0"
 
-args = ["tinygo", "build", "-o", "../dist/s2-bindings.wasm"]
-if not DEBUG:
-    args.extend(["-no-debug", "-panic", "trap", "-opt", "2"])
+    wasm_path = os.path.join(tempdir, "s2-bindings.wasm")
 
-subprocess.check_call(
-    args,
-    env={"GOOS": "wasi1p", "GOARCH": "wasm", **os.environ},
-)
-with open("../dist/s2-bindings.wasm", "rb") as f:
-    wasm = f.read()
-    with open("../src/component/lib/s2wasm.js", "wb") as f:
-        f.write(b'export const wasmSource = "')
-        f.write(base64.b64encode(wasm))
-        f.write(b'";\n')
+    args = ["tinygo", "build", "-o", wasm_path]
+    if not DEBUG:
+        args.extend(["-no-debug", "-panic", "trap", "-opt", "2"])
+
+    subprocess.check_call(
+        args,
+        env={"GOOS": "wasi1p", "GOARCH": "wasm", **os.environ},
+    )
+    with open(wasm_path, "rb") as f:
+        wasm = f.read()
+        with open("../component/lib/s2wasm.js", "wb") as f:
+            f.write(b'export const wasmSource = "')
+            f.write(base64.b64encode(wasm))
+            f.write(b'";\n')
